0a49c0fc489d9a434df172edb50db88734e13a48
l1141693575 <1141693575@qq.com>
2019-09-07 12:03:41 +0800

    //代码生成器

---
+++ code-parent/src/main/java/com/code/constant/Constant.java
@@ -0,0 +1,35 @@
+package com.code.constant;
+
+/**
+ * Created by dm.
+ */
+public class Constant {
+
+    public static String template_path = "D:\\code-parent\\src\\main\\resources\\dm";
+
+    //实体model类生成地址
+    public static String model_save_path = "D:\\evapotranspiration\\second_common\\second_module\\src\\main\\java\\com\\zb\\pojo";
+
+    public static String mapper_save_path = "D:\\evapotranspiration\\second_common\\second_dao\\src\\main\\resources\\mapper";
+    public static String class_mapper_save_path = "D:\\evapotranspiration\\second_common\\second_dao\\src\\main\\java\\com\\zb\\mapper";
+
+    public static String client_dm_save_path = "D:\\evapotranspiration\\second_common\\second_client\\src\\main\\java\\com\\zb\\client";
+    public static String client_dm_fallback_save_path = "D:\\evapotranspiration\\second_common\\second_client\\src\\main\\java\\com\\zb\\fallback";
+
+
+
+    public static String service_dm_user_save_path = "D:\\evapotranspiration\\new_user\\news_order_provider\\src\\main\\java\\com\\zb\\service";
+    //item provider路径
+//    public static String service_dm_item_save_path = "E:\\javawork\\dm-item-provider\\src\\main\\java\\cn\\dm\\service";
+    //order provider路径
+//    public static String service_dm_order_save_path = "E:\\javawork\\dm-order-provider\\src\\main\\java\\cn\\dm\\service";
+    //pay provider路径
+//    public static String service_dm_pay_save_path = "E:\\javawork\\dm-pay-provider\\src\\main\\java\\cn\\dm\\service";
+    //scheduler provider路径
+//    public static String service_dm_scheduler_save_path = "E:\\javawork\\dm-scheduler-provider\\src\\main\\java\\cn\\dm\\service";
+    //user provider路径
+    //common provider路径
+//    public static String service_dm_common_save_path = "E:\\javawork\\dm-base-provider\\src\\main\\java\\cn\\dm\\service";
+
+
+}
